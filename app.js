e3595d7a95becd59ed7766113ae02928664a9c23
Toby Lorne <tlornewr@gmail.com>
2015-11-01 00:20:47 -0400

    Update design with map and new font

---
+++ app.js
@@ -0,0 +1,7 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibWFjcm9tZWFzdXJlcyIsImEiOiI1OGNlZmM1MzdkZWU2MTIzMTU0ODJlNmEyMzhlMTk4NSJ9.AcwpBr0WGuA4essSTZ7SmQ';
+L.mapbox.map('map', 'macromeasures.o20ddp0k', {
+  zoomControl: false,
+  infoControl: false,
+  legendControl: false,
+  attributionControl: false,
+});
