b29bf84b0bf515bfc58ddc67561d15080df08cb7
toby lorne <toby@toby.codes>
2020-08-01 22:34:11 +0100

    k8s: deploy & monitor petitions-exporter
    release label is v important for service monitor
    
    kubectl -n prometheus system describe prometheus-tobys-cloud... shows
    that there is a label selector on the service monitor
    
    Signed-off-by: toby lorne <toby@toby.codes>

---
+++ k8s/metrics/apply.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+
+wd="$(cd "$(dirname $0)" ; pwd)"
+
+ns="$(operator-tools yaml find -p /namespace < "$wd/meta.yaml")"
+
+kubectl apply \
+  --namespace "$ns" \
+  -f "$wd/01-namespace.yaml"
+
+kubectl apply \
+  --namespace "$ns" \
+  -f "$wd/02-petitions-exporter.yaml"
+
+kubectl apply \
+  --namespace prometheus-system \
+  -f "$wd/03-service-monitors.yaml"
