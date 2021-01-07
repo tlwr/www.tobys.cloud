8e3867abd50684fd5208bdf6d56dbbe2d60c124d
tlwr <toby@toby.codes>
2021-01-07 20:57:23 +0000

    what-is-my-ip.tobys.cloud: add wildcard path
    otherwise /foo times out
    
    Signed-off-by: tlwr <toby@toby.codes>

--- workers/what-is-my-ip/main.tf
+++ workers/what-is-my-ip/main.tf
@@ -28,6 +28,6 @@ resource "cloudflare_worker_script" "what_is_my_ip" {
 
 resource "cloudflare_worker_route" "what_is_my_ip_tobys_cloud" {
   zone_id = local.tobys_cloud_zone_id
-  pattern = "what-is-my-ip.tobys.cloud"
+  pattern = "what-is-my-ip.tobys.cloud/*"
   script_name = cloudflare_worker_script.what_is_my_ip.name
 }
