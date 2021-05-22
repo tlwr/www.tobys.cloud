7cc6b4220c1dda6aec2a6f23e7774b5175620d93
toby lorne <toby@toby.codes>
2021-05-22 12:26:22 +0200

    vultr: add terraform provider
    Signed-off-by: toby lorne <toby@toby.codes>

--- vultr/terraform/main.tf
+++ vultr/terraform/main.tf
@@ -6,4 +6,15 @@ terraform {
       name = "www-tobys-cloud"
     }
   }
+
+  required_providers {
+    vultr = {
+      source  = "vultr/vultr"
+      version = "2.3.0"
+    }
+  }
+}
+
+provider "vultr" {
+  api_key = var.vultr_api_key
 }
