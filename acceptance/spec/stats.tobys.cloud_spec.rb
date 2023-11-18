1adee3c90c83548a734d6fece558a16ba63cb46c
toby lorne <toby@toby.codes>
2023-11-18 20:29:46 +0100

    acceptance: add stats.tobys.cloud
    Signed-off-by: toby lorne <toby@toby.codes>

---
+++ acceptance/spec/stats.tobys.cloud_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+def stats_url
+  'https://stats.tobys.cloud/'
+end
+
+describe 'stats.tobys.cloud', type: :feature do
+  it 'has charts on the page' do
+    page = agent.get(stats_url)
+    expect(page.code.to_i).to eq(200)
+
+    svgs = page.css('svg')
+    expect(svgs.count).to be == 15, 'should render svgs (max 15) for each petition'
+
+    petitions = page.css('tr')
+    petitions.each do |petition|
+      next unless petition.css('th').empty? # skip table heading
+      name = petition.css('td').first.text.strip
+      path = petition.css('path').first
+      expect(path.attr('d').length).to be > 100, "petition #{name} must have long svg path, short path is broken chart"
+    end
+  end
+end
