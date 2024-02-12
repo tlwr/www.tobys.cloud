b8dd4c6ae92ee46084f601d1ac9eda5346474239
toby lorne <toby@toby.codes>
2024-02-12 19:43:16 +0100

    acceptance: add discord webhook reporter
    Signed-off-by: toby lorne <toby@toby.codes>

--- acceptance/spec/spec_helper.rb
+++ acceptance/spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'rspec'
 require 'irb'
+require 'time'
 
+require 'http'
 require 'mechanize'
 
 RSpec.shared_context 'http' do
@@ -13,4 +15,17 @@ end
 
 RSpec.configure do |config|
   config.include_context 'http'
+
+  config.after :suite do
+    duration_s = Time.now - config.start_time
+    tally = config.reporter.examples.map { _1.execution_result.status }.tally
+    msg = "www.tobys.cloud acceptance tests duration #{duration_s.round(2)}s #{tally.to_a.join(" ")}"
+
+    webhook_url = ENV['DISCORD_WEBHOOK_URL']
+    if Random.rand(0...60) == 0 && webhook_url
+      HTTP.post(webhook_url, :json => { :content => msg })
+    else
+      puts msg
+    end
+  end
 end
